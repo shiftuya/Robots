0c316317cea6109bbbb58ee6ed1a40540a3323c8
KyMaKa <seafth345@gmail.com>
2019-12-26 00:21:47 +0700

    Small changes & addet new method

--- DataBase/src/main/java/i/shatalov/teamproject/database/updateDB.java
+++ DataBase/src/main/java/i/shatalov/teamproject/database/updateDB.java
@@ -29,12 +29,12 @@ public class updateDB {
             String sql = "SELECT id FROM login";
             ResultSet resultSet = stmt.executeQuery(sql);
             int ifCheck = 0;
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 ifCheck = resultSet.getInt("id");
-            }
-            if (ifCheck != id) {
-                System.out.println("Record with given id already exist");
-                return;
+                if (ifCheck == id) {
+                    System.out.println("Record with given id already exist");
+                    return;
+                }
             }
             System.out.println("Inserting given records in the table...");
             stmt = conn.createStatement();
@@ -58,6 +58,7 @@ public class updateDB {
                 se.printStackTrace();
             }//end finally try
         }//end try
+        System.out.println("--END--");
     }
 
 
@@ -101,6 +102,7 @@ public class updateDB {
                 se.printStackTrace();
             }//end finally try
         }//end try
+        System.out.println("--END--");
         return askedRecord;
     }
 
@@ -140,6 +142,44 @@ public class updateDB {
                 se.printStackTrace();
             }//end finally try
         }//end try
+        System.out.println("--END--");
     }
+    public void deleteUser(int id) {
 
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected database successfully.");
+
+            System.out.println("Creating statement...");
+
+            stmt = conn.createStatement();
+            String sql = "DELETE FROM login WHERE id = " + id;
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("User with id = " + id + "successfully deleted");
+        System.out.println("--END--");
+    }
 }
--- DataBase/src/main/java/i/shatalov/teamproject/database/useDB.java
+++ DataBase/src/main/java/i/shatalov/teamproject/database/useDB.java
@@ -3,12 +3,16 @@ package i.shatalov.teamproject.database;
 public class useDB {
     public static void main(String[]args) {
         updateDB changeDB = new updateDB();
-        changeDB.insertNewRecords(100,
-                "s.morozov",
-                "morozz",
+        changeDB.insertNewRecords(102,
+                "spykex3",
+                "robot",
                 "Student");
         System.out.println(changeDB.takeRecord(100,"LoginName"));
-        changeDB.updateRecord(100, "LoginName", "Morozzz.S");
+        changeDB.updateRecord(100, "LoginName", "S.Morozzz");
         System.out.println(changeDB.takeRecord(100,"LoginName"));
+        System.out.println(changeDB.takeRecord(102, "LoginName"));
+        System.out.println(changeDB.takeRecord(102, "Password"));
+        System.out.println(changeDB.takeRecord(102, "AccountType"));
+        changeDB.deleteUser(102);
     }
 }
