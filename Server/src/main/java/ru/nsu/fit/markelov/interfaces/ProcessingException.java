7a4df7c9baa68123472ae8d7db45ffcdf0c6075e
omarkelov <36790281+omarkelov@users.noreply.github.com>
2020-03-29 15:33:29 +0700

    ProcessingException fix

--- Server/src/main/java/ru/nsu/fit/markelov/interfaces/ProcessingException.java
+++ Server/src/main/java/ru/nsu/fit/markelov/interfaces/ProcessingException.java
@@ -1,8 +1,8 @@
 package ru.nsu.fit.markelov.interfaces;
 
 /**
- * ProcessingException class is used to throw an exception with message which is supposed be shown
- * in the client app.
+ * ProcessingException class is used to throw an exception with message which is supposed to be
+ * shown in the client app.
  */
 public class ProcessingException extends RuntimeException {
 
